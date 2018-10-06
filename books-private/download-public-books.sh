bd9c45c0f517341d6fe51afd4fc01b164edeb461
Аббас Гусенов <gusenov@live.ru>
2018-10-06 22:49:01 +0600

    Установка куки в скриптах для загрузки открытых книг и их последующего скрытия.

--- books-private/download-public-books.sh
+++ books-private/download-public-books.sh
@@ -1,25 +1,67 @@
 #!/bin/bash
 
-for i in "$@"
-do
+# Usage:
+#  $ "./download-public-books.sh" -u="YOUR_USER_NAME" -l="2048" -o="public-books.json" -c="ipp_key=YOUR_IPP_KEY; ipp_uid=YOUR_IPP_UID;"
+
+for i in "$@"; do
 case $i in
-    -u=*|--user=*)
+  -u=*|--user=*)
     user="${i#*=}"
     shift # past argument=value
     ;;
-    -l=*|--limit=*)
+  -l=*|--limit=*)
     limit="${i#*=}"
     shift # past argument=value
     ;;
-    --default)
+  -c=*|--cookie=*)
+    cookie_value="${i#*=}"
+    shift # past argument=value
+    ;;
+  -o=*|--output=*)
+    output_file="${i#*=}"
+    shift # past argument=value
+    ;;
+  --default)
     user=""
     limit=2048
+    cookie_value="ipp_key=YOUR_IPP_KEY; ipp_uid=YOUR_IPP_UID;"
+    output_file="public-books.json"
     shift # past argument with no value
     ;;
-    *)
-          # unknown option
+  *)
+    echo "Unknown option!"
+    exit
     ;;
 esac
 done
 
-wget -O "public-books.json" "https://bookmate.com/p/api/v5/users/$user/library_cards?page=1&per_page=$limit"
+Accept="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+Accept_Encoding="gzip, deflate, br"
+Accept_Language="en,en-US;q=0.9,ru;q=0.8,az;q=0.7"
+Cache_Control="max-age=0"
+Connection="keep-alive"
+Cookie="$cookie_value"
+DNT="1"
+Host="bookmate.com"
+Upgrade_Insecure_Requests="1"
+User_Agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/69.0.3497.81 Chrome/69.0.3497.81 Safari/537.36"
+
+echo "Open page and Get cookie:"
+echo "https://bookmate.com/p/api/v5/users/$user/library_cards?page=1&per_page=$limit"
+
+[ -e "$output_file" ] && rm "$output_file"
+
+wget --quiet \
+     --header="Accept: $Accept" \
+     --header="Accept-Encoding: $Accept_Encoding" \
+     --header="Accept-Language: $Accept_Language" \
+     --header="Cache-Control: $Cache_Control" \
+     --header="Connection: $Connection" \
+     --header="Cookie: $Cookie" \
+     --header="DNT: $DNT" \
+     --header="Host: $Host" \
+     --header="Upgrade-Insecure-Requests: $Upgrade_Insecure_Requests" \
+     --user-agent="$User_Agent" \
+     -O "$output_file" "https://bookmate.com/p/api/v5/users/$user/library_cards?page=1&per_page=$limit"
+
+gunzip < "$output_file" | jq . | sponge "$output_file"
--- books-private/make-books-private.sh
+++ books-private/make-books-private.sh
@@ -1,21 +1,21 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Usage:
-#  $ "./make-books-private.sh" --bms=""
+#  $ "./make-books-private.sh" --cookie="YOUR_IPP_KEY; ipp_uid=YOUR_IPP_UID; bms=YOUR_BMS"
 
-for i in "$@"
-do
+for i in "$@"; do
 case $i in
-    -b=*|--bms=*)
-    bms="${i#*=}"
+  -c=*|--cookie=*)
+    cookie_value="${i#*=}"
     shift # past argument=value
     ;;
-    --default)
-    bms=""
+  --default)
+    bms="YOUR_COOKIE_BMS"
     shift # past argument with no value
     ;;
-    *)
-          # unknown option
+  *)
+    echo "Unknown option!"
+    exit
     ;;
 esac
 done
@@ -24,14 +24,15 @@ done
 
 Accept="application/json"
 Accept_Encoding="gzip, deflate, br"
-Accept_Language="en,en-US;q=0.8,ru;q=0.6,az;q=0.4"
+Accept_Language="en,en-US;q=0.9,ru;q=0.8,az;q=0.7"
 Connection="keep-alive"
 Content_Type="application/json"
-Cookie="bms=$bms"
+Cookie="$cookie_value"
 DNT="1"
 Host="bookmate.com"
 Origin="https://bookmate.com"
-User_Agent="Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36"
+#User_Agent="Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36"
+User_Agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/69.0.3497.81 Chrome/69.0.3497.81 Safari/537.36"
 
 input_file="public-books.json"
 
@@ -39,7 +40,7 @@ limit=$(jq '.library_cards | length' "$input_file")
 for ((i = 0; i < $limit; i++)); do
     library_card_uuid=$(jq --raw-output '.library_cards['$i'].uuid' "$input_file")
     book_uuid=$(jq --raw-output '.library_cards['$i'].book.uuid' "$input_file")
-    
+
     Referer="https://bookmate.com/books/$book_uuid"
     response=$(curl -s -X PUT "https://bookmate.com/p/api/v5/profile/library_cards/$library_card_uuid" -d '{"lc":{"uuid":"'$library_card_uuid'","public":false}}' \
         --header "Accept: $Accept" \
@@ -55,6 +56,6 @@ for ((i = 0; i < $limit; i++)); do
         --header "Referer: $Referer" \
         --header "User-Agent: $User_Agent")
     #echo $response
-    
+
     echo -n "$((1 + i)) of $limit; "
 done
